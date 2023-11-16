4b556c245dfbbbe6cf6c3e3e7696b99d951f1690
PK Owusu-Ansah <kwesta96@gmail.com>
2023-11-16 22:30:21 +0200

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -20,7 +20,7 @@ export const Home = () => {
     <div className='home' id='home'>
       <div className="wrapper">
           <h2 className="h2">Hey there, I'm</h2>
-          <h1 className="h1">PK Owusu-Ansah</h1>
+          <h1 className="h1">Kwesi Owusu</h1>
           <h3 className="h3">And I'm a <span ref={textRef}></span></h3>
       </div>
       <a href="#work">
